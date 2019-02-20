09ba924af48e521c9a0a96eae3d2817f71bf94ee
qmonmous <q.monmousseau@gmail.com>
2019-02-20 03:08:50 +0100

    adding multicolinearity engineering

---
+++ Code-Snippets/Engineering/check_multicolinearity.py
@@ -0,0 +1,14 @@
+#Colinearity is the state where two variables are highly correlated and contain similiar information about the variance within a given dataset.
+#To detect colinearity among variables, we simply create a correlation matrix
+import seaborn as sns
+
+#Make sure you work only on features X
+sns.heatmap(data=X.corr(), annot=True).set_title('Corrélations des différentes variables')
+
+#To choose what the features we should remove, we'll use Variance Inflation Factor (VIF).
+#We will onky keep all features if VIF < 5. If not, we'll remove the feature with the highest VIF and then run the snippet again.
+
+vif = pd.DataFrame()
+vif["VIF Factor"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+vif["features"] = X.columns
+vif.round(1)
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Machine-Learning-Algorithms-in-Python
 This repository aims at going through different machine learning algorithms and providing some (re)usable codes in Python.
 
-# Table of Contents
+## Table of Contents
 1. [Articles](https://github.com/qmonmous/ENGLISH--Machine-Learning-X-Python/tree/master/Articles) to learn about Data Science
 2. [Case Studies](https://github.com/qmonmous/ENGLISH--Machine-Learning-X-Python/tree/master/Case-Studies) to go through different Machine Learning problems
 3. [Code Snippets](https://github.com/qmonmous/ENGLISH--Machine-Learning-X-Python/tree/master/Code-Snippets) to pick some code snippets
